bac37f63aa352cc8ea62189e580d27ebd80ea365
FelipeReis1 <feliperg15@yahoo.com.br>
2022-10-15 22:18:46 -0300

    versao 3 - botao fechar pedido aparece apos selecionar 3 produtos

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-function selecionarPedido(seletor){
+let pedidoUm;
+let pedidoDois;
+let pedidoTres;
+
+function selecionarPedido(seletor) {
 
     const selecaoAnterior = document.querySelector('.pedidos .selecionado');
-    if(selecaoAnterior !== null){
+    if (selecaoAnterior !== null) {
         selecaoAnterior.classList.remove('selecionado');
     }
-   seletor.classList.add('selecionado');
+    seletor.classList.add('selecionado');
+    pedidoUm = 1;
+    fecharPedido();
 }
 
-function selecionarPedidoDois(seletor){
+function selecionarPedidoDois(seletor) {
 
     const selecaoAnterior = document.querySelector('.pedidosDois .selecionado');
-    if(selecaoAnterior !== null){
+    if (selecaoAnterior !== null) {
         selecaoAnterior.classList.remove('selecionado');
     }
-   seletor.classList.add('selecionado');
+    seletor.classList.add('selecionado');
+    pedidoDois = 1;
+    fecharPedido();
 }
 
-function selecionarPedidoTres(seletor){
+function selecionarPedidoTres(seletor) {
 
     const selecaoAnterior = document.querySelector('.pedidosTres .selecionado');
-    if(selecaoAnterior !== null){
+    if (selecaoAnterior !== null) {
         selecaoAnterior.classList.remove('selecionado');
     }
-   seletor.classList.add('selecionado');
+    seletor.classList.add('selecionado');
+    pedidoTres = 1;
+    fecharPedido();
+}
+
+function fecharPedido() {
+
+    if(pedidoUm !== undefined && pedidoDois !== undefined && pedidoTres!== undefined){
+        const seletorClasse = document.querySelector('.menuInferiorVerde');
+        seletorClasse.classList.remove('escondido');
+    }
+    
+
+
 }
