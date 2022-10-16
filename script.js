f946a0a82f93e675dc83ac08ef14f12c2961ec86
FelipeReis1 <feliperg15@yahoo.com.br>
2022-10-15 21:39:42 -0300

    seletor independente de pedidos implementado

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function selecionarPedido(seletor){
 
-    const selecaoAnterior = document.querySelector('.selecionado');
+    const selecaoAnterior = document.querySelector('.pedidos .selecionado');
+    if(selecaoAnterior !== null){
+        selecaoAnterior.classList.remove('selecionado');
+    }
+   seletor.classList.add('selecionado');
+}
+
+function selecionarPedidoDois(seletor){
+
+    const selecaoAnterior = document.querySelector('.pedidosDois .selecionado');
+    if(selecaoAnterior !== null){
+        selecaoAnterior.classList.remove('selecionado');
+    }
+   seletor.classList.add('selecionado');
+}
+
+function selecionarPedidoTres(seletor){
+
+    const selecaoAnterior = document.querySelector('.pedidosTres .selecionado');
     if(selecaoAnterior !== null){
         selecaoAnterior.classList.remove('selecionado');
     }
